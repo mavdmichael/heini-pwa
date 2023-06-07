e92419ebd10bd21f181cc0780486b3c273e0eeb3
mavdmichael <michael+github@marknadsavdelningen.se>
2023-06-07 16:32:57 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="scroll-wrapper">
+        <main>hej</main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
